cb14b0c6bb32e3769c87a358e822e80179cd9c8b
Maira Medrano <mairamedrano28@gmail.com>
2023-05-10 14:10:33 -0500

    feat: added new feature loginonly dummy test data object

--- server.js
+++ server.js
@@ -1,14 +1,46 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors')
+const app = express();
+// const path = require('path');
+// const basicAuth = require('express-basic-auth');
 
 const {PORT = 3030} = process.env;
+// const {PORT = 3001} = process.env;
+// const PORT = process.env.port || 3001;
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+app.use(cors());
+app.use(express.json())
 
-// app.listen(3030)
+const data = {
+    users: [
+        {
+            id: 1,
+            email: 'useremail@email.com',
+            password: 'password1',
+            token: 'test123'
+        }
+    ]
+}
+
+app.use('/user/signin', (req, res) => {
+    if (req.body.email === data.users[0].email && req.body.password === data.users[0].password) {
+        res.send({
+            token: data.users[0].token
+        });
+    } else {
+        res.status(400)
+    }
+
+});
+
+// app.get('/', function (req, res) {
+//     res.send('Hello World')
+// })
+
+
 app.listen(PORT, function (err) {
     if (err) console.log(err);
     console.log("Server listening on PORT", PORT);
 });
+
+// app.listen(3001, () => console.log('API is running on port ' + PORT));
