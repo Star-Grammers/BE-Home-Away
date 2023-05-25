7bc431702cd9281340117195dd0c7fc94bdbe963
Maira Medrano <mairamedrano28@gmail.com>
2023-05-25 12:34:42 -0400

    fix:cleanup

--- server.js
+++ server.js
@@ -1,42 +1,151 @@
-const express = require('express');
-const cors = require('cors')
+const dotenv = require("dotenv");
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+const testData = require("./testData.json");
+
+dotenv.config();
 const app = express();
 
-const {PORT = 3030} = process.env;
+const { PORT = 3030, DB_URI } = process.env;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-const data = {
-    users: [
-        {
-            id: 1,
-            email: 'useremail@email.com',
-            password: 'password1',
-            token: 'test123'
-        }
-    ]
+mongoose
+  .connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Failed to connect to MongoDB:", error);
+  });
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+const User = mongoose.model("User", userSchema);
+
+const reservationSchema = new mongoose.Schema({
+  guestCount: {
+    type: Number,
+    required: true,
+  },
+  petsCount: {
+    type: Number,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  checkInDate: {
+    type: Date,
+    required: true,
+  },
+});
+
+const Reservation = mongoose.model("Reservation", reservationSchema);
+
+app.post("/api/reservations", async (req, res) => {
+  const { guestCount, petsCount, phoneNumber, checkInDate } = req.body;
+
+  try {
+    const newReservation = new Reservation({
+      guestCount,
+      petsCount,
+      phoneNumber,
+      checkInDate,
+    });
+
+    await newReservation.save();
+    console.log(newReservation, "the newReservation");
+    res.status(200).json({ message: "Reservation saved successfully" });
+  } catch (error) {
+    res.status(500).send("An error occurred");
+  }
+});
+
+app.get("/api/reservations", async (req, res) => {
+  try {
+    const reservations = await Reservation.find({});
+    res.json(reservations);
+  } catch (error) {
+    res.status(500).send("An error occurred");
+  }
+});
+
+app.get("/", async (req, res) => {
+  try {
+    res.send("Hello, world!").status(200);
+  } catch (error) {
+    res.status(500).send("An error occurred");
+  }
+});
+
+app.post("/user/signup", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      res.status(409).send("User already exists");
+    } else {
+      const newUser = new User({ email, password });
+      await newUser.save();
+
+      res.send({ message: "User created successfully" });
+    }
+  } catch (error) {
+    res.status(500).send("An error occurred");
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.json(users);
+  } catch (error) {
+    res.status(500).send("An error occurred");
+  }
+});
+
+app.post("/user/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user || user.password !== password) {
+      res.status(401).send("Invalid email or password");
+    } else {
+      res.send({ token: user.token });
+    }
+  } catch (error) {
+    res.status(500).send("An error occurred");
+  }
+});
+
+app.get("/api/listings", (req, res) => {
+  res.json(testData);
+});
+
+function errorHandler(err) {
+  if (err) console.log(err);
+  console.log("Server listening on PORT", PORT);
 }
 
-app.use('/user/signin', (req, res) => {
-    if (req.body.email === data.users[0].email && req.body.password === data.users[0].password) {
-        res.send({
-            token: data.users[0].token
-        });
-    } else {
-        res.status(400)
-    }
+app.listen(PORT, errorHandler);
 
-});
-
-//need to use mongo db and implement proper post/get requests
-// app.get('/', function (req, res) {
-//     res.send('Hello World')
-// })
-
-
-app.listen(PORT, function (err) {
-    if (err) console.log(err);
-    console.log("Server listening on PORT", PORT);
-});
-
+module.exports = User;
