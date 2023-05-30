70805eb93c5f9362dad1c59fa075172701cfc110
Maira Medrano <mairamedrano28@gmail.com>
2023-05-30 14:24:28 -0400

    Keep calm and commit

--- routes.js
+++ routes.js
@@ -1,86 +1,13 @@
-// import dotenv from "dotenv";
 import cors from "cors";
 import express from "express";
-// import mongoose from "mongoose";
 import x from "./testData.js";
-import {User, Reservation
-} from "./models/Post.js";
-// console.log(User, 'hello')
+import { User, Reservation } from "./models/Post.js";
+
 const router = express.Router();
-
-// dotenv.config();
-// const app = express();
-
-// const { PORT = 3030, DB_URI } = process.env;
-
-// app.use(cors());
-// app.use(express.json());
 
 router.use(cors());
 router.use(express.json());
 
-// mongoose
-//   .connect(DB_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("Connected to MongoDB");
-//   })
-//   .catch((error) => {
-//     console.error("Failed to connect to MongoDB:", error);
-//   });
-
-// const userSchema = new mongoose.Schema({
-//   email: {
-//     type: String,
-//     required: true,
-//   },
-//   password: {
-//     type: String,
-//     required: true,
-//   },
-// });
-// const User = mongoose.model("User", userSchema);
-
-// const reservationSchema = new mongoose.Schema({
-//   guestCount: {
-//     type: Number,
-//     required: true,
-//   },
-//   petsCount: {
-//     type: Number,
-//     required: true,
-//   },
-//   phoneNumber: {
-//     type: String,
-//     required: true,
-//   },
-//   checkInDate: {
-//     type: Date,
-//     required: true,
-//   },
-// });
-
-// const Reservation = mongoose.model("Reservation", reservationSchema);
-
-// app.post("/api/reservations", async (req, res) => {
-//   const { guestCount, petsCount, phoneNumber, checkInDate } = req.body;
-
-//   try {
-//     const newReservation = new Reservation({
-//       guestCount,
-//       petsCount,
-//       phoneNumber,
-//       checkInDate,
-//     });
-
-//     await newReservation.save();
-//     res.status(200).json({ message: "Reservation saved successfully" });
-//   } catch (error) {
-//     res.status(500).send("An error occurred");
-//   }
-// });
 router.post("/api/reservations", async (req, res) => {
   const { guestCount, petsCount, phoneNumber, checkInDate } = req.body;
 
@@ -100,15 +27,6 @@ router.post("/api/reservations", async (req, res) => {
   }
 });
 
-// app.get("/api/reservations", async (req, res) => {
-//   try {
-//     const reservations = await Reservation.find({});
-//     res.json(reservations);
-//   } catch (error) {
-//     res.status(500).send("An error occurred");
-//   }
-// });
-
 router.get("/api/reservations", async (req, res) => {
   try {
     const reservations = await Reservation.find({});
@@ -118,13 +36,6 @@ router.get("/api/reservations", async (req, res) => {
   }
 });
 
-// app.get("/", async (req, res) => {
-//   try {
-//     res.send("Hello, world!").status(200);
-//   } catch (error) {
-//     res.status(500).send("An error occurred");
-//   }
-// });
 router.get("/", async (req, res) => {
   try {
     res.send("Hello, world!").status(200);
@@ -132,25 +43,6 @@ router.get("/", async (req, res) => {
     res.status(500).send("An error occurred");
   }
 });
-
-// app.post("/user/signup", async (req, res) => {
-//   const { email, password } = req.body;
-
-//   try {
-//     const existingUser = await User.findOne({ email });
-
-//     if (existingUser) {
-//       res.status(409).send("User already exists");
-//     } else {
-//       const newUser = new User({ email, password });
-//       await newUser.save();
-
-//       res.send({ message: "User created successfully" });
-//     }
-//   } catch (error) {
-//     res.status(500).send("An error occurred");
-//   }
-// });
 
 router.post("/user/signup", async (req, res) => {
   const { email, password } = req.body;
@@ -167,44 +59,20 @@ router.post("/user/signup", async (req, res) => {
       res.send({ message: "User created successfully" });
     }
   } catch (error) {
-    // res.status(500).send("This is your error:", error);
-   res.status(500).send(`This is your error: ${error}`);
+    res.status(500).send(`This is your error: ${error}`);
   }
 });
 
-// app.get("/users", async (req, res) => {
-//   try {
-//     const users = await User.find({});
-//     res.json(users);
-//   } catch (error) {
-//     res.status(500).send("An error occurred");
-//   }
-// });
 router.get("/users", async (req, res) => {
   try {
     const users = await User.find({});
     res.json(users);
-// res.status(200).send(users)
+    // res.status(200).send(users)
   } catch (error) {
     res.status(500).send("An error occurred");
   }
 });
 
-// app.post("/user/login", async (req, res) => {
-//   const { email, password } = req.body;
-
-//   try {
-//     const user = await User.findOne({ email });
-
-//     if (!user || user.password !== password) {
-//       res.status(401).send("Invalid email or password");
-//     } else {
-//       res.send({ token: user.token });
-//     }
-//   } catch (error) {
-//     res.status(500).send("An error occurred");
-//   }
-// });
 router.post("/user/login", async (req, res) => {
   const { email, password } = req.body;
 
@@ -214,7 +82,7 @@ router.post("/user/login", async (req, res) => {
     if (!user || user.password !== password) {
       res.status(401).send("Invalid email or password");
     } else {
-    res.status(200).send(user)
+      res.status(200).send(user);
       // res.send({ token: user.token });
     }
   } catch (error) {
@@ -222,20 +90,8 @@ router.post("/user/login", async (req, res) => {
   }
 });
 
-// app.get("/api/listings", (req, res) => {
-//   res.json(x);
-// });
-
 router.get("/api/listings", (req, res) => {
   res.json(x);
 });
 
-// function errorHandler(err) {
-//   if (err) console.log(err);
-//   console.log("Server listening on PORT", PORT);
-// }
-
-// app.listen(PORT, errorHandler);
-
-// export default app;
 export default router;
--- server.test.js
+++ server.test.js
@@ -1,49 +1,17 @@
-// import request from "supertest";
-// import app from "./routes";
-
-// describe("GET /", () => {
-//   test("responds with status code 200 and correct response body", async () => {
-//     const response = await request(app).get("/");
-//     expect(response.statusCode).toBe(200);
-//   }, 10000);
-
-//   test("responds with correct response body", async () => {
-//     const response = await request(app).get("/");
-//     expect(response.text).toBe("Hello, world!");
-//   }, 10000);
-// });
-
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-// import request from "supertest";
 import supertest from "supertest";
-import createServer from "./server";
-// import UserModel from "./models/Post.js";
+import createServer from "./server.js";
 
 dotenv.config();
 const { DB_URI } = process.env;
 
-// beforeEach((done) => {
-//   mongoose.connect(
-//     "mongodb://localhost:27017/test",
-//     { useNewUrlParser: true },
-//     () => done()
-//   );
-// });
-
-// afterEach((done) => {
-//   mongoose.connection.db.dropDatabase(() => {
-//     mongoose.connection.close(() => done());
-//   });
-// });
 beforeEach(async () => {
   try {
     await mongoose.connect(DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-
-    // console.log("Server runing");
   } catch (error) {
     console.error("Error connecting to the MongoDB database:", error);
   }
@@ -59,23 +27,11 @@ afterEach(async () => {
 });
 
 const app = createServer();
-// console.log(app, "app mf");
 test("GET /", async () => {
-  // const userModel = await UserModel.create({
-  //   email: "ismael@gmail.com",
-
-  //   password: "123",
-  // });
-
   await supertest(app)
     .get("/")
     .expect(200)
     .then((response) => {
       expect(response.text).toBe("Hello, world!");
-      // expect(response.body.length).toEqual(1);
-
-      // expect(response.body[0]._id).toBe(post.id);
-      // expect(response.body[0].email).toBe(userModel.email);
-      // expect(response.body[0].password).toBe(userModel.password);
     });
 });
