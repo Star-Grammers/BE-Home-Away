826406b02a110ba87d48722fa491ace574bdf1e9
Maira Medrano <mairamedrano28@gmail.com>
2023-06-04 14:50:11 -0400

    fix: remove unneccessary code, comments

--- models/Post.js
+++ models/Post.js
@@ -17,25 +17,4 @@ const userSchema = new mongoose.Schema({
 
 export const User = mongoose.model("User", userSchema);
 
-// const reservationSchema = new mongoose.Schema({
-//   guestCount: {
-//     type: Number,
-//     required: true,
-//   },
-//   petsCount: {
-//     type: Number,
-//     required: true,
-//   },
-//   phoneNumber: {
-//     type: String,
-//     required: true,
-//   },
-//   checkInDate: {
-//     type: Date,
-//     required: true,
-//   },
-// });
-
-// export const Reservation = mongoose.model("Reservation", reservationSchema);
-// export default { User, Reservation};
 export default { User };
